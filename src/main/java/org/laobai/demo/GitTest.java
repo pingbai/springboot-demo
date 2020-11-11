4d60ecbcc084f66329cb318f407036e9c332051b
pingbai <1831347339@qq.com>
2020-11-11 16:54:47 +0800

    Update GitTest.java
    这是别人的第一个任务

--- src/main/java/org/laobai/demo/GitTest.java
+++ src/main/java/org/laobai/demo/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("jiusan v-1.0");
         System.out.println("这是我的第一个任务");
+         System.out.println("这是别人的第一个任务");
     }
 }
